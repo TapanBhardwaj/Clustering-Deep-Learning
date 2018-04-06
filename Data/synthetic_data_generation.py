b1d8847b1912b08126e35af470411d9107dd134e
TapanBhardwaj <tapanbhardwaj93@gmail.com>
2018-04-06 15:41:06 +0530

    Done refactoring

--- Data/synthetic_data_generation.py
+++ Data/synthetic_data_generation.py
@@ -1,26 +1,18 @@
-import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
 
-def syn_data_points(n_samples=10000, centers=[(-5, -5), (5, -5), (-5, 5), (5, 5)]):
+def syn_data_points(n_samples=10000):
     """
     returns 2-Dimensional data points centers around the given centers
-    and saves the plot
 
     :param n_samples:
-    :param centers:
     :return:
     """
+    # centers of the clusters
+    centers = [(-5, -5), (5, -5), (-5, 5), (5, 5)]
+
     x, y = make_blobs(n_samples=n_samples, n_features=2, cluster_std=1.0,
                       centers=centers, shuffle=False, random_state=42)
-
-    # saves the plot of the synthetic data points
-    plt.scatter(x[:, 0], x[:, 1], edgecolors=(0, 0, 0))
-    plt.xlabel("h0")
-    plt.ylabel("h1")
-    plt.grid()
-    plt.title("2 Dimensional Synthetic data")
-    plt.savefig("../synthetic_data_points.png")
 
     return x, y
 
--- Data/synthetic_plots.py
+++ Data/synthetic_plots.py
@@ -1,93 +1,96 @@
-import random
-from synthetic_data_generation import syn_data_points
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.decomposition import PCA
 
-#Assumming data of 4-class
-def plot_points(x,y):
-    x1=list()
-    x2=list()
-    x3=list()
-    x4=list()
-    y1=list()
+from synthetic_data_generation import syn_data_points
+
+
+# Assuming data of 4-class
+def plot_points(x, y):
+    x1 = list()
+    x2 = list()
+    x3 = list()
+    x4 = list()
+    y1 = list()
     y2 = list()
     y3 = list()
     y4 = list()
     for i in range(y.shape[0]):
-        if(y[i]==1):
-            x1.append(x[i,0])
-            y1.append(x[i,1])
-        elif(y[i]==2):
-            x2.append(x[i,0])
-            y2.append(x[i,1])
-        elif (y[i] == 3):
+        if y[i] == 1:
+            x1.append(x[i, 0])
+            y1.append(x[i, 1])
+        elif y[i] == 2:
+            x2.append(x[i, 0])
+            y2.append(x[i, 1])
+        elif y[i] == 3:
             x3.append(x[i, 0])
             y3.append(x[i, 1])
         else:
-            x4.append(x[i,0])
-            y4.append(x[i,1])
-    #print(x.shape)
+            x4.append(x[i, 0])
+            y4.append(x[i, 1])
 
-    plt.scatter(x1,y1,color=['red'],label='Cluster 1')
-    plt.scatter(x2, y2, color=['green'], label='Cluster 2')
-    plt.scatter(x3, y3, color=['blue'], label='Cluster 3')
-    plt.scatter(x4, y4, color=['orange'], label='Cluster 4')
+    plt.scatter(x1, y1, color=['red'], label='Cluster 1', edgecolors=(0, 0, 0))
+    plt.scatter(x2, y2, color=['green'], label='Cluster 2', edgecolors=(0, 0, 0))
+    plt.scatter(x3, y3, color=['blue'], label='Cluster 3', edgecolors=(0, 0, 0))
+    plt.scatter(x4, y4, color=['orange'], label='Cluster 4', edgecolors=(0, 0, 0))
     plt.legend()
     plt.title('Plot of data points')
-    #plt.show()
 
 
-#Assigning cluster no. based on coordinate system
+# Assigning cluster no. based on coordinate system
 def assign_cluster(x):
-    if(x[0]>=0 and x[1]>=0):
+    if x[0] >= 0 and x[1] >= 0:
         return 1
-    elif(x[0]<0 and x[1]>=0):
+    elif x[0] < 0 and x[1] >= 0:
         return 2
-    elif(x[0]<0 and x[1]<0):
+    elif x[0] < 0 and x[1] < 0:
         return 3
     else:
         return 4
 
-#Generating 2-d points and dividing them in 4-clusters based on co-ordinate system
 
+# Generating 2-d points and dividing them in 4-clusters based on co-ordinate system
 
-
-#sigmoid calculation
 def sigmoid(z):
-    z_hat=1/(1+np.exp(-z))
+    """
+    sigmoid calculation
+    :param z:
+    :return:
+    """
+    z_hat = 1 / (1 + np.exp(-z))
     return z_hat
 
 
-#transform data from 2-d to 100-d using non-linear transformation
-#assume data points to be 2-dimension
-#x=sigmoid(U*sigmoid(W*x(in 2d)))
-def to_higer_dimension(x):
-    W=np.random.normal(loc=0,scale=1,size=(10,2))
-    #U=np.random.normal(loc=0,scale=1,size=(100,10))
-    x_new=np.square(sigmoid(np.dot(W,x.T))).T
-    #print(x_new.shape)
+def to_higher_dimension(x):
+    """
+    transform data from 2-d to 100-d using non-linear transformation
+    assume data points to be 2-dimension
+    x=sigmoid(U*sigmoid(W*x(in 2d)))
+
+    :param x:
+    :return:
+    """
+    W = np.random.normal(loc=0, scale=1, size=(10, 2))
+    # U=np.random.normal(loc=0,scale=1,size=(100,10))
+    x_new = np.square(sigmoid(np.dot(W, x.T))).T
     return x_new
 
 
-def to_higer_dimension2(x):
-    W=np.random.normal(loc=0,scale=1,size=(10,2))
-    #U=np.random.normal(loc=0,scale=1,size=(100,10))
-    x_new=np.tanh(sigmoid(np.dot(W,x.T))).T
-    #print(x_new.shape)
+def to_higher_dimension2(x):
+    W = np.random.normal(loc=0, scale=1, size=(10, 2))
+    # U=np.random.normal(loc=0,scale=1,size=(100,10))
+    x_new = np.tanh(sigmoid(np.dot(W, x.T))).T
     return x_new
+
 
 if __name__ == '__main__':
     x, y = syn_data_points()
     plot_points(x, y)
     plt.savefig("../data_points_2_dim.png")
     plt.close()
-    x_high = to_higer_dimension2(x)
+    x_high = to_higher_dimension2(x)
 
     pca = PCA(n_components=2)
     x_pca = pca.fit_transform(x_high)
     plot_points(x_pca, y)
     plt.savefig("../data_points_100_dim.png")
-
-
-
